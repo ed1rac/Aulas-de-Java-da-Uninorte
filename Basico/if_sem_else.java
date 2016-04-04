6e90e3928f7cfdb1c3dd71b63a756a54819bbfa7
ed1rac <edkallenn@gmail.com>
2016-04-04 18:58:14 -0500

    Primeiro programa do capítulo 3 - if sem else

---
+++ Basico/if_sem_else.java
@@ -0,0 +1,18 @@
+/*	Programa: if-sem-else.java - pasta básico
+	Função: Mostra o uso do if sem o else
+	Data: 23/05/2014
+	Autor: Edkallenn
+	Observações: Java 7 - ED - p. 49 com alterações do professor
+*/
+import javax.swing.JOptionPane;
+public class if_sem_else {
+    public static void main(String[] args) {
+        String resposta = JOptionPane.showInputDialog("Digite sua resposta: S (SIM) ou N (NÃO)");
+        String men = "Letra Fornecida: " + resposta;
+        if (!resposta.equals("S") && !resposta.equals("N")){
+            men = men + " é inválida! ";
+        }
+        JOptionPane.showMessageDialog(null, men);
+        System.exit(0);
+    }
+}
